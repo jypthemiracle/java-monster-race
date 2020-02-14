4eecf0bd89a2277fab2d832e5057e6755cf9c986
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-14 19:53:09 +0900

    [MODIFY] IMPLEMENTING METHODS TO TEST DELETE/MODIFY/SHOW/MENU FEATURES DETAILS FOR FOLLOWING: 1. (loadGameTest, loadGameCRUDMenu methods) Trying to implement verify method of mockito framework for grasping the status whether method calls another method. 2. Add showAllMonstersInfoTest with utilizing for iterative statment that holds assertTrue method in order for checking the output contains the necessary information including the name of monsters, the number of monster's movement. 3. Add modifyMonsterInfoTest that harnesses the feature of assertEquals. The method works by implementing the array that states the name of monsters concerned, and two desirable infomation to change the data. 4. Add deleteMonsterTest with capitalizing on assertNotEquals features by comparing the element through iterating all elements of arraylist. NEEDS TO LEARN FURTHER: 1. JUnit 5 DOES NOT support @Test(expected = IllegalArgumentException.class) annotation. Why deprecated? What else for utilizing the feature? 2. String toGenericString() : which is the method to return the information of the specific method that includes type parameter. 3. The difference between charSequence and String. NEEDS TO BE IMPROVED: 1. Hard-coded in loadGameCRUDMenuTest method. Not sure it gonna works.

--- MonsterRace/src/test/java/MonsterTest.java
+++ MonsterRace/src/test/java/MonsterTest.java
@@ -1,26 +1,115 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import controller.Controller;
+import controller.Database;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import model.Flier;
 import model.Monsters;
 import model.Psychic;
 import model.Runner;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.jupiter.api.Test;
 import utils.DragonUtils;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import view.OutputView;
 
 public class MonsterTest {
 
-  Psychic psychic = new Psychic("Psychic");
-  Runner runner = new Runner("Runner");
-  Flier flier = new Flier("Flier");
+  private Monsters psychic;
+  private Monsters flier;
+  private Monsters runner;
+
+  static void setup() {
+    Psychic psychic = new Psychic("Javajigi");
+    Runner runner = new Runner("Jason");
+    Flier flier = new Flier("Brian");
+  }
 
   @Test
-  public void WHETHER_RANDOMNUMBER_BETWEEN_0_AND_9() {
+  public void loadGameTest()
+      throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    Controller controller = mock(Controller.class);
+    controller.loadGame(1); //User가 1을 입력했다고 가정합니다.
+    verify(controller).startGame(); //startGame이 로딩되었는지 확인합니다.
+
+    controller.loadGame(2); //User가 2를 입력했다고 가정합니다.
+    verify(controller).loadGameCRUDMenu(3); //loadGameCRUDMenu이 로딩되었는지 확인합니다.
+  }
+
+  @Test
+  public void loadGameCRUDMenuTest()
+      throws IOException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    Controller controller = mock(Controller.class);
+    String testName = "honux";
+    String changeName = "JK";
+    String changeType = "Flier";
+    String[] modifyInfo = new String[]{testName, changeName, changeType};
+
+    controller.loadGameCRUDMenu(1);
+    verify(controller).showAllMonstersInfo();
+
+    controller.loadGameCRUDMenu(2);
+    verify(controller).modifyMonstersInfo(modifyInfo);
+
+    controller.loadGameCRUDMenu(3);
+    verify(controller).addNewMonstersInfo();
+
+    controller.loadGameCRUDMenu(4);
+    verify(controller).deleteMonstersInfo(testName);
+
+    controller.loadGameCRUDMenu(5);
+    verify(controller).loadGame(1); //1번 실행해보는 것으로 지정
+  }
+
+  @Test
+  public void showAllMonstersInfoTest() {
+    Controller controller = mock(Controller.class);
+    OutputView outputview = mock(OutputView.class);
+
+    controller.showAllMonstersInfo();
+    verify(outputview).printAllMonsters();
+
+    int size = Database.monstersData.size();
+    for (int i = 0; i < size; i++) {
+      assertTrue(outputview.printAllMonsters().contains(Database.monstersData.get(i).getName()));
+      assertTrue(outputview.printAllMonsters().contains(Database.monstersData.get(i).getMoveNum()));
+    }
+  }
+
+  @Test
+  public void modifyMonsterInfoTest()
+      throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+    Controller controller = mock(Controller.class);
+    String testName = "honux";
+    String changeName = "JK";
+    String changeType = "Flier";
+    String[] modifyInfo = new String[]{testName, changeName, changeType};
+
+    Database.monstersData.add(new Runner(testName));
+    controller.modifyMonstersInfo(modifyInfo);
+    assertEquals(changeName, Database.monstersData.get(0).getName()); //이름이 바뀌었는가?
+    assertEquals(changeType, Database.monstersData.get(0).getClass().toString()); //타입이 바뀌었는가?
+    //String toGenericString() : 타입 매개변수를 포함한 해당 메소드의 정보를 리턴
+  }
+
+  @Test
+  public void deleteMonsterTest() {
+    Controller controller = mock(Controller.class);
+    String deleteName = "honux";
+    controller.deleteMonstersInfo(deleteName);
+    for (int i = 0; i < Database.monstersData.size(); i++) {
+      assertNotEquals(Database.monstersData.get(i).getName(), deleteName);
+    }
+  }
+
+  @Test
+  public void WHETHER_RANDOM_NUMBER_BETWEEN_0_AND_9() {
     for (int i = 0; i < 999; i++) {
       int random = DragonUtils.getRandomNumber();
       assertTrue(random >= 0 && random < 10);
@@ -29,17 +118,20 @@ public class MonsterTest {
 
   @Test
   public void TO_MOVE_WHEN_UPPERTHAN_RANDOMTHRESHOLD() {
+    setup();
     psychic.updateResults(9);
     flier.updateResults(6);
     runner.updateResults(4);
 
-    assertEquals(3, flier.getMoveNum());
-    assertEquals(1, runner.getMoveNum());
-    assertTrue(psychic.getMoveNum() < 100 && psychic.getMoveNum() > 0);
+    assertEquals(3, Integer.parseInt(flier.getMoveNum().toString()));
+    assertEquals(1, Integer.parseInt(runner.getMoveNum().toString()));
+    assertTrue(Integer.parseInt(psychic.getMoveNum().toString()) < 100
+        && Integer.parseInt(psychic.getMoveNum().toString()) > 0);
   }
 
   @Test
   public void SORTING_BY_POSITION_DESC() {
+    setup();
     psychic.setMove(9);
     flier.setMove(6);
     runner.setMove(4);
@@ -58,19 +150,20 @@ public class MonsterTest {
   }
 
   @Test
-  public void TEST_TOSTRING(){
+  public void TEST_TOSTRING() {
+    setup();
     psychic.setMove(9);
     flier.setMove(6);
     runner.setMove(4);
 
     assertEquals("Psychic 9---------" + "\n", psychic.toString());
-    assertEquals("Flier 6------" + "\n",flier.toString());
+    assertEquals("Flier 6------" + "\n", flier.toString());
     assertEquals("Runner 4----" + "\n", runner.toString());
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void SPLIT_WITH_DELIMITER_TEST() {
-    DragonUtils.splitWithDelimiter(",", ""); //performs adequately
-    DragonUtils.splitWithDelimiter(null, ""); //expected to throw exception
-  }
+//  //@Test(expected = IllegalArgumentException.class)
+//  public void SPLIT_WITH_DELIMITER_TEST() {
+//    DragonUtils.splitWithDelimiter(",", ""); //performs adequately
+//    DragonUtils.splitWithDelimiter(null, ""); //expected to throw exception
+//  }
 }
